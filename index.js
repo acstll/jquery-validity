046550b2f2ad7099b19b42f2138eaa31cf4df606
Greg Slepak <contact@taoeffect.com>
2016-06-07 15:20:45 -0700

    Refactoring + custom errorClass + attempt to address #1, #3

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-/* global jQuery */
+/* global define */
 
 (function (factory) {
-  if (typeof module === 'object' && typeof module.exports === 'object') {
-    factory(require('jquery'), global, global.document)
+  if (typeof exports === 'object') {
+    module.exports = factory(require('jquery')) // node / ES6 module
+  } else if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory)                 // requirejs
   } else {
-    factory(jQuery, window, document)
+    factory(window.jQuery)                      // <script> tag in HTML
   }
-}(factory))
-
-function factory ($, window, document) {
-  var fields = []
-
+})(function ($) {
   // Testing
   // window.__fields = fields
 
@@ -34,6 +32,7 @@ function factory ($, window, document) {
    */
   $.fn.validity = function (options) {
     var settings = $.extend({}, $.fn.validity.defaults, options)
+    var fields = []
 
     if (!settings.validators.required) {
       settings.validators.required = required(settings.requiredMessage)
@@ -52,6 +51,7 @@ function factory ($, window, document) {
     attributeName: 'data-validators',
     requiredMessage: 'This field is required',
     parentSelector: 'p',
+    errorClass: 'error',
     timeout: 1000, // if `false`, no "live" validation
     onSubmit: null,
     validateOnBlur: true,
@@ -122,18 +122,21 @@ function factory ($, window, document) {
    */
   function listen (form, fields, settings) {
     var handleDelayed = debounce(function (field) {
-      handle(field)
+      handle(field, settings)
     }, settings.timeout)
     var $form = $(form)
 
     $form
       .on('submit', function onSubmit (event) {
-        var firstErrorIndex = validateAll(fields)
+        var firstErrorIndex = validateAll(fields, settings)
 
         if (firstErrorIndex !== null) {
           event.preventDefault()
           fields[firstErrorIndex].el.focus()
-          $form.triggerHandler('validity.invalid', { fields: fields.slice(0) })
+          // TODO: fix Sprint.js trigger implementation:
+          //       - needs triggerHandler
+          //       - needs to accept an object/params
+          $form.trigger('validity.invalid', { fields: fields.slice(0) })
         } else if (typeof settings.onSubmit === 'function') {
           event.preventDefault()
           settings.onSubmit(event, form)
@@ -158,179 +161,166 @@ function factory ($, window, document) {
       var el = event.target
 
       if (settings.validateOnBlur !== false && el.__validity) {
-        handle(el.__validity)
+        handle(el.__validity, settings)
       }
     }
   }
-}
 
-/**
- * Validate the entire form by calling {@link handle}
- * on every field.
- *
- * @param {array<field>} fields
- * @returns {number|undefined} `null` if the form is valid, otherwise the index of
- * the first field with an error
- * @private
- */
-function validateAll (fields) {
-  var index, isValid
-  fields.forEach(handle)
+  /**
+   * Validate the entire form by calling {@link handle}
+   * on every field.
+   *
+   * @param {array<field>} fields
+   * @returns {number|undefined} `null` if the form is valid, otherwise the index of
+   * the first field with an error
+   * @private
+   */
+  function validateAll (fields, settings) {
+    var index, isValid
+    fields.forEach(function (field) { handle(field, settings) })
 
-  isValid = !fields.some(function (field, i) {
-    index = i
-    return !field.isValid
-  })
-
-  return isValid ? null : index
-}
-
-/**
- * Validate a single field. This sets the `isValid` property on the field
- * object and calls {@link updateElements}.
- *
- * @param {field} field
- * @returns {boolean} `true` if valid, `false` is not
- * @private
- */
-function handle (field) {
-  var value = getValue(field)
-  var message = validate(field, value)
-  var isValid = message === null
-
-  field.isValid = isValid
-  updateElements(field, message)
-
-  return isValid
-}
-
-/**
- * Extract the value of a field, normally the input.value.
- * For radio or checkbox input groups, it will return an empty string
- * when no input is checked.
- *
- * @param {field} field
- * @returns {string}
- * @private
- */
-function getValue (field) {
-  var el = field.el
-  var type = el.type
-  var checked
-
-  if (field.$els.length > 1 || type === 'radio' || type === 'checkbox') {
-    checked = field.$els.filter(function (index, el) {
-      return el.checked
+    isValid = !fields.some(function (field, i) {
+      index = i
+      return !field.isValid
     })
-    return checked.length ? el.value : ''
-  }
-  return el.value.trim()
-}
 
-/**
- * Define the validator functions to be used in a field.
- * This is used by {@link createField}
- *
- * @param {HTMLElement} el
- * @param {Object} settings
- * @returns {array<Function>} An array of validator functions
- * @private
- */
-function getValidators (el, settings) {
-  var data = el.getAttribute(settings.attributeName) || ''
-  var keys = data.split(' ')
-
-  if (el.required) {
-    keys.unshift('required')
+    return isValid ? null : index
   }
 
-  return keys
-    .map(function (key) {
-      return settings.validators[key]
-    })
-    .filter(function (fn) {
-      return typeof fn === 'function'
-    })
-}
+  /**
+   * Validate a single field. This sets the `isValid` property on the field
+   * object and calls {@link updateElements}.
+   *
+   * @param {field} field
+   * @returns {boolean} `true` if valid, `false` is not
+   * @private
+   */
+  function handle (field, settings) {
+    var value = getValue(field)
+    var message = validate(field, value)
+    var isValid = message === null
+    var action = isValid ? 'removeClass' : 'addClass'
 
-/**
- * Update the DOM of a field according to validity.
- *
- * @param {field} field
- * @param {string} message The error message
- * @returns {undefined}
- * @private
- */
-function updateElements (field, message) {
-  var action = field.isValid ? 'removeClass' : 'addClass'
+    field.isValid = isValid
+    field.$parent[action](settings.errorClass)
+    field.$error.text(message || '')
 
-  field.$parent[action]('error')
-  field.$error.text(message || '')
-}
-
-/**
- * Validate a single field by running all of its validator functions
- * agaist the field’s value.
- *
- * @param {field} field
- * @param {string} value
- * @returns {undefined|string} `null` if valid, a string with error message otherwise
- * @private
- */
-function validate (field, value) {
-  var validators = field.validators
-  var result = null
-
-  if (validators && validators.length) {
-    validators.some(function (fn) {
-      result = fn(value, field.name, field.el)
-      if (result !== null) {
-        return true
-      }
-    })
+    return isValid
   }
 
-  return result
-}
+  /**
+   * Extract the value of a field, normally the input.value.
+   * For radio or checkbox input groups, it will return an empty string
+   * when no input is checked.
+   *
+   * @param {field} field
+   * @returns {string}
+   * @private
+   */
+  function getValue (field) {
+    var el = field.el
+    var type = el.type
+    var checked
 
-/**
- * The default `required` validator implementation.
- *
- * @param {string} message
- * @returns {Function}
- * @private
- */
-function required (message) {
-  return function (value, name) {
-    if (!value || value === '') {
-      return message
+    if (field.$els.length > 1 || type === 'radio' || type === 'checkbox') {
+      checked = field.$els.filter(function (index, el) {
+        return el.checked
+      })
+      return checked.length ? el.value : ''
     }
-    return null
-  }
-}
-
-/**
- * Debounce utility.
- *
- * @param {Function} fn
- * @param {number|false} delay
- * @returns {Function}
- * @author Remy Sharp
- * @private
- */
-function debounce (fn, delay) {
-  var timer = null
-
-  if (delay === false) {
-    return fn
+    return el.value.trim()
   }
 
-  return function () {
-    var context = this
-    var args = arguments
-    clearTimeout(timer)
-    timer = setTimeout(function () {
-      fn.apply(context, args)
-    }, delay)
+  /**
+   * Define the validator functions to be used in a field.
+   * This is used by {@link createField}
+   *
+   * @param {HTMLElement} el
+   * @param {Object} settings
+   * @returns {array<Function>} An array of validator functions
+   * @private
+   */
+  function getValidators (el, settings) {
+    var data = el.getAttribute(settings.attributeName) || ''
+    var keys = data.split(' ')
+
+    if (el.required) {
+      keys.unshift('required')
+    }
+
+    return keys
+      .map(function (key) {
+        return settings.validators[key]
+      })
+      .filter(function (fn) {
+        return typeof fn === 'function'
+      })
   }
-}
+
+  /**
+   * Validate a single field by running all of its validator functions
+   * agaist the field’s value.
+   *
+   * @param {field} field
+   * @param {string} value
+   * @returns {undefined|string} `null` if valid, a string with error message otherwise
+   * @private
+   */
+  function validate (field, value) {
+    var validators = field.validators
+    var result = null
+
+    if (validators && validators.length) {
+      validators.some(function (fn) {
+        result = fn(value, field.name, field.el)
+        if (result !== null) {
+          return true
+        }
+      })
+    }
+
+    return result
+  }
+
+  /**
+   * The default `required` validator implementation.
+   *
+   * @param {string} message
+   * @returns {Function}
+   * @private
+   */
+  function required (message) {
+    return function (value, name) {
+      if (!value || value === '') {
+        return message
+      }
+      return null
+    }
+  }
+
+  /**
+   * Debounce utility.
+   *
+   * @param {Function} fn
+   * @param {number|false} delay
+   * @returns {Function}
+   * @author Remy Sharp
+   * @private
+   */
+  function debounce (fn, delay) {
+    var timer = null
+
+    if (delay === false) {
+      return fn
+    }
+
+    return function () {
+      var context = this
+      var args = arguments
+      clearTimeout(timer)
+      timer = setTimeout(function () {
+        fn.apply(context, args)
+      }, delay)
+    }
+  }
+})
